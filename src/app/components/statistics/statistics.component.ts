39a78b57a6f5af36906f332b1eb2f0b4b8ec3a4d
Kirito <kurulluvarov@gmail.com>
2024-08-22 14:12:36 +0300

    В статиску добавлено время самого долгого вопроса

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -19,25 +19,27 @@ export class StatisticsComponent implements OnInit{
   public statistic = ''
   public arrayTimeQuestions:Array<number> = [1, 3, 4, 6, 7, 8, 9, 10, 11, 13]
   public arithmeticMean=0
+  public hardQuestion = 0
   constructor(public data:DataService) {
 
   }
   ngOnInit(): void {
     this.getStatistics()
     console.log(this.statistic)
-    this.arithmeticTimeQuestion()
+    this.statisticTimeQuestion()
     console.log(this.arithmeticMean)
   }
 public getStatistics(): void{
   this.statistic = this.data.getStatistic()
 }
-public arithmeticTimeQuestion():void{
+public statisticTimeQuestion():void{
       let array = []
      array.push(this.arrayTimeQuestions[0])
      for(let i = 0;i < this.arrayTimeQuestions.length-1;i++){
        array.push(this.arrayTimeQuestions[i+1]-this.arrayTimeQuestions[i])
      }
-     this.arithmeticMean=array.reduce((acc,el)=>acc+el,0)/this.arrayTimeQuestions.length
+    this.hardQuestion = Math.max(...array)
+    this.arithmeticMean = array.reduce((acc,el)=>acc+el,0)/this.arrayTimeQuestions.length
   console.log(array)
   }
 }
