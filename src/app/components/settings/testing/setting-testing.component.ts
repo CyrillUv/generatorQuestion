87c1607acaf444f82cb3776fb9bd4df8df56dd19
Kirito <kurulluvarov@gmail.com>
2024-10-11 18:17:11 +0300

    Исправление функционала компонента select

--- src/app/components/settings/testing/setting-testing.component.ts
+++ src/app/components/settings/testing/setting-testing.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
 import { SettingsComponent } from '../settings.component';
 
 import { NgForOf, NgIf, NgTemplateOutlet } from '@angular/common';
 import { SidebarComponent } from '../../custom/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { SelectComponent } from '../../custom/select/select.component';
 
 @Component({
   selector: 'app-setting-testing',
   styleUrl: '../settings.component.scss',
   templateUrl: 'setting-testing.component.html',
-  imports: [RouterLink, NgIf, NgForOf, SidebarComponent, NgTemplateOutlet],
+  imports: [
+    RouterLink,
+    NgIf,
+    NgForOf,
+    SidebarComponent,
+    NgTemplateOutlet,
+    FormsModule,
+    SelectComponent,
+  ],
   standalone: true,
 })
-export class SettingTestingComponent extends SettingsComponent {}
+export class SettingTestingComponent extends SettingsComponent {
+  @ViewChild('optionRef') option!: TemplateRef<unknown>;
+  public selectHandler(option: number): void {
+    this.activeBlockTests = option;
+    this.ms.setActiveBlockTests(option);
+  }
+}
