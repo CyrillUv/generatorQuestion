ea3ecb4712e940a142e0cb123b21ba88358d7e78
Kirito <kurulluvarov@gmail.com>
2024-10-15 13:55:50 +0300

    Добавление custom select component

---
+++ src/app/components/custom/select/select.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForOf } from '@angular/common';
+import { MenuService } from '../../../data/menu/menu.service';
+import { IDataMenu } from '../../../data/menu/data-menu';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [FormsModule, NgForOf],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.css',
+})
+export class SelectComponent implements OnInit {
+  @Input() public activeBlockTests!: number;
+  @Output() public selectedBlockTestsEmitter = new EventEmitter<number>();
+  @Input() public dataMenu!: IDataMenu[];
+  public selectedOption = 1; // Переменная для хранения выбранного option
+  constructor(public ms: MenuService) {}
+
+  ngOnInit(): void {
+    this.dataMenu = this.ms.getData();
+  }
+  public testsHandler() {
+    this.selectedBlockTestsEmitter.emit(this.selectedOption);
+  }
+}
