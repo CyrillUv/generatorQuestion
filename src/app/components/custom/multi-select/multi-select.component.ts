0ac771299b25e27dff5a2e3510532ad5d5b4600a
Kirito <kurulluvarov@gmail.com>
2024-10-30 01:15:35 +0300

    Дописан custom multiselect,  категория ts закрыта для дополнения

--- src/app/components/custom/multi-select/multi-select.component.ts
+++ src/app/components/custom/multi-select/multi-select.component.ts
@@ -12,27 +12,57 @@ import { FormsModule } from '@angular/forms';
 export class MultiSelectComponent {
   public options: string[] = [
     'Опция 1',
-    'Опция 2',
+    'Опция 242',
     'Опция 3',
-    'Опция 4',
-    'Опция 5',
+    'Опция 45',
+    'Опция 57',
     'Опция 6',
     'Опция 7',
     'Опция 8',
   ];
+  public searchOptions: string[] = this.options;
   public selectedOptions: string[] = [];
   public activeSelect = false;
+  public allSelect = false;
+  public searchField = '';
 
   public openSelect(): void {
     this.activeSelect = !this.activeSelect;
   }
-
+  public allOptions(): void {
+    this.allSelect = !this.allSelect;
+    if (this.allSelect) {
+      this.selectedOptions = this.searchOptions;
+    }
+    if (!this.allSelect) {
+      this.selectedOptions = [];
+    }
+  }
   public addOption(option: string): void {
+    console.log(this.selectedOptions);
     if (this.selectedOptions.includes(option)) {
       this.selectedOptions = this.selectedOptions.filter((el) => el !== option);
     } else {
       this.selectedOptions.push(option);
       this.selectedOptions = this.selectedOptions.map((option) => option);
     }
+  }
+
+  public filterOptions() {
+    if (!this.searchField) {
+      this.searchOptions = this.options;
+      return;
+    }
+    this.searchOptions = this.options.filter(
+      (el) => el.toLowerCase().indexOf(this.searchField.toLowerCase()) > -1,
+    );
+  }
+
+  public removeOptions() {
+    this.searchOptions = this.options;
+    this.selectedOptions = [];
+    this.activeSelect = false;
+    this.allSelect = false;
+    this.searchField = '';
   }
 }
