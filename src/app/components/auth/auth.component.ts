de2b87ea7705a4e965fb964e52a9e1e458eb45d1
Kirito <kurulluvarov@gmail.com>
2024-12-02 19:56:47 +0300

    Начало добавления раздела js,появление возможности восстановления пароля

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,13 +1,12 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { ToastService } from '../custom/toast/toast.service';
 import { ToastStatus } from '../custom/toast/toast.component';
-import {MenuService} from "../../data/menu/menu.service";
-import {BehaviorSubject, timer} from "rxjs";
-import {AUTHORIZATION_TOKEN} from "../../data/tokens/tokens";
-import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
+import { MenuService } from '../../data/menu/menu.service';
+import { BehaviorSubject, timer } from 'rxjs';
+import { AUTHORIZATION_TOKEN } from '../../data/tokens/tokens';
 
 @Component({
   selector: 'app-auth',
@@ -18,11 +17,16 @@ import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 })
 export class AuthComponent implements OnInit {
   public isRegistration = false;
-  public isOblivion = {isPassword:false,isLogin:false,isChangePassword:false}
-  public credForLogin = { login: '', password: ''};
-  public credForRegistration = { login: '', password: '',secretWord:'' };
+  public isOblivion = {
+    isPassword: false,
+    isLogin: false,
+    isChangePassword: false,
+  };
+  public credForLogin = { login: '', password: '' };
+  public credForRegistration = { login: '', password: '', secretWord: '' };
   public readonly minLengthChar = 4;
   public showPassword = false;
+
   constructor(
     private router: Router,
     private toastService: ToastService,
@@ -31,19 +35,24 @@ export class AuthComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    localStorage.setItem('isLogin','false');
+    localStorage.setItem('isLogin', 'false');
     this.authToken$.next(false);
   }
-
-  public showHiddenPassword():void{
-    this.showPassword=!this.showPassword;
+  public getStorage(key:string):string{
+    return localStorage.getItem(key) as string
   }
+
+  public showHiddenPassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
   public onRegistration(): void {
     if (
       this.credForRegistration.login.trim().length >= this.minLengthChar &&
-      this.credForRegistration.password.trim().length >= this.minLengthChar&&this.credForRegistration.secretWord.trim().length
+      this.credForRegistration.password.trim().length >= this.minLengthChar &&
+      this.credForRegistration.secretWord.trim().length
     ) {
-      if (localStorage.getItem(this.credForRegistration.login)) {
+      if (this.getStorage(this.credForRegistration.login)) {
         this.toastService.openToast({
           title: 'Информация',
           type: ToastStatus.info,
@@ -62,7 +71,7 @@ export class AuthComponent implements OnInit {
         });
         this.isRegistration = false;
       }
-    }else{
+    } else {
       this.toastService.openToast({
         title: 'Информация',
         type: ToastStatus.info,
@@ -70,6 +79,7 @@ export class AuthComponent implements OnInit {
       });
     }
   }
+
   public successLogin(): void {
     const time =
       new Date().getHours() +
@@ -106,15 +116,21 @@ export class AuthComponent implements OnInit {
       this.authToken$.next(false);
     });
   }
-  public onForgotLogin(): void {
-
-  }
-  public reductionPassword():void{
-    if(this.credForLogin.password===this.credForRegistration.password){
-    localStorage.setItem(this.credForLogin.login,JSON.stringify({...this.credForLogin}));
-
-    this.isToggle(false)}
-    else{
+  public reductionPassword(): void {
+    console.log(this.credForLogin.password ,'log',this.credForRegistration.password,'reg')
+    if (this.credForLogin.password === this.credForRegistration.password) {
+      const newAccount = {login:this.credForLogin.login,password:this.credForRegistration.password, secretWord:this.credForRegistration.secretWord};
+      localStorage.setItem(
+        this.credForLogin.login,
+        JSON.stringify(newAccount),
+      );
+      this.toastService.openToast({
+        title: 'Успех!',
+        type: ToastStatus.success,
+        description: 'Пароль успешно изменён!',
+      });
+      this.isToggle(false);
+    } else {
       this.toastService.openToast({
         title: 'Ошибка!',
         type: ToastStatus.warning,
@@ -122,38 +138,78 @@ export class AuthComponent implements OnInit {
       });
     }
   }
-  public onForgotPassword(): void {
-    console.log(JSON.parse(localStorage.getItem(this.credForLogin.login) as string).secretWord !== this.credForRegistration.secretWord)
 
-    console.log(JSON.parse(localStorage.getItem(this.credForLogin.login) as string).secretWord !== this.credForRegistration.secretWord)
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    JSON.parse(localStorage.getItem(this.credForLogin.login) as string).secretWord === this.credForRegistration.secretWord&&
-    JSON.parse(localStorage.getItem(this.credForLogin.login) as string).login === this.credForLogin.login
-      ?
-      (this.isToggle(false),this.isOblivion.isChangePassword = true)
-      : this.toastService.openToast({
+  public onForgotPassword(): void {
+    if (
+      !this.credForRegistration.secretWord.length ||
+      !this.credForLogin.login.length
+    ) {
+      this.toastService.openToast({
+        title: 'Информация!',
+        type: ToastStatus.info,
+        description: 'Заполните все поля ввода!',
+      });
+      return;
+    }
+    if (
+      this.getStorage(this.credForLogin.login) === null ||
+      this.getStorage(this.credForRegistration.secretWord) === null
+    ) {
+      this.toastService.openToast({
         title: 'Ошибка!',
         type: ToastStatus.warning,
-        description: 'Неправильный ввод данных!',
+        description: 'Неправильный логин или секретное слово!',
       });
+    }
+    if (
+      JSON.parse(this.getStorage(this.credForLogin.login)) &&
+      JSON.parse(this.getStorage(this.credForLogin.login))
+        .secretWord.trim() === this.credForRegistration.secretWord.trim() &&
+      JSON.parse(this.getStorage(this.credForLogin.login))
+        .login.trim() === this.credForLogin.login.trim()
+    ) {
+      this.isOblivion.isChangePassword = true;
+    }
   }
+
   public onLogin(): void {
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    JSON.parse(localStorage.getItem(this.credForLogin.login) as string).login === this.credForLogin.login&&
-    JSON.parse(localStorage.getItem(this.credForLogin.login) as string).password===this.credForLogin.password
-      ? this.successLogin()
-      : this.toastService.openToast({
-          title: 'Ошибка!',
-          type: ToastStatus.warning,
-          description: 'Неправильный логин или пароль!',
-        });
+    if (
+      !this.credForLogin.login.trim().length ||
+      !this.credForLogin.password.trim().length
+    ) {
+      this.toastService.openToast({
+        title: 'Информация!',
+        type: ToastStatus.info,
+        description: 'Заполните все поля ввода',
+      });
+
+    }
+    if (
+      this.getStorage(this.credForLogin.login) === null ||
+      this.getStorage(this.credForLogin.password) === null
+    ) {
+      this.toastService.openToast({
+        title: 'Ошибка!',
+        type: ToastStatus.warning,
+        description: 'Неправильный логин или пароль!',
+      });
+
+    }
+    if (this.getStorage(this.credForLogin.login)&&
+      JSON.parse(this.getStorage(this.credForLogin.login))
+        .login === this.credForLogin.login &&
+      JSON.parse(this.getStorage(this.credForLogin.login))
+        .password === this.credForLogin.password
+    ) {
+      this.successLogin();
+    }
   }
 
   public isToggle(registration: boolean): void {
-    this.credForRegistration={login: '',password: '',secretWord: ''};
-    this.credForLogin={login: '',password: ''};
+    this.credForRegistration = { login: '', password: '', secretWord: '' };
+    this.credForLogin = { login: '', password: '' };
     this.isRegistration = registration;
-    this.isOblivion.isPassword=false;
-    this.isOblivion.isLogin=false
+    this.isOblivion.isPassword = false;
+    this.isOblivion.isLogin = false;
   }
 }
